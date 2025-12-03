9d81b84907800896594d3108c580008caf701db2
LutfiHaryaFerdian <lutfiharyaferdian@gmail.com>
2025-12-03 23:55:42 +0700

    add fitur

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request
 from rules import SYMPTOMS, diagnose
+from rules import SYMPTOMS, FAULTS
 
 app = Flask(__name__)
+
+@app.route("/gejala")
+def gejala_page():
+    return render_template("gejala.html", symptoms=SYMPTOMS)
+
+@app.route("/kerusakan")
+def kerusakan_page():
+    return render_template("kerusakan.html", faults=FAULTS)
+
+@app.route("/tentang")
+def tentang_page():
+    return render_template("tentang.html")
 
 @app.route("/")
 def index():
     return render_template("index.html", symptoms=SYMPTOMS)
+
+@app.route("/diagnose-start")
+def diagnose_start():
+    return render_template("form.html", symptoms=SYMPTOMS)
 
 @app.route("/diagnose", methods=["POST"])
 def do_diagnose():
